6b87646bf8ec9f6aef13437dd571c141b6d82a7f
daxp472 <daxpatel.cg@gmail.com>
2025-03-14 23:12:49 +0530

    Input component added

--- Frontend/src/components/Resume-Template/Input.jsx
+++ Frontend/src/components/Resume-Template/Input.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 
-const Input = ({ value, onChange, placeholder, className = '', onBlur, ...props }) => {
-  return (
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      onBlur={onBlur}
-      className={`w-full p-2 border rounded ${className}`}
-      {...props}
-    />
-  );
-};
+const Input = ({ className = '', ...props }) => (
+  <input
+    className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all ${className}`}
+    {...props}
+  />
+);
 
 export default Input;
